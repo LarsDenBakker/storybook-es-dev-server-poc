7003452cfbaf585e9e3e7f82be82d5bce5627450
Lars den Bakker <larsdenbakker@gmail.com>
2019-11-03 21:48:27 +0100

    Fix watch mode

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,23 @@
 #!/usr/bin/env node
-const { createConfig, startServer } = require('es-dev-server');
+const { createConfig, startServer, messageChannelEndpoint } = require('es-dev-server');
 const readCommandLineArgs = require('./command-line-args');
-const createServeStorybookTransformer = require('./transformers/serve-storybook');
+const createServeManagerMiddleware = require('./middleware/serve-manager');
+const createServePreviewTransformer = require('./transformers/serve-preview');
 const mdxToJSTransformer = require('./transformers/mdx-to-js');
+const createAssets = require('./create-assets');
 
 const config = readCommandLineArgs();
+const storybookConfigDir = config.storybookServerConfig['storybook-config'];
+const assets = createAssets({ storybookConfigDir });
+
+config.esDevServerConfig.middlewares = [
+  createServeManagerMiddleware(assets),
+  ...(config.esDevServerConfig.middlewares || []),
+];
 
 config.esDevServerConfig.responseTransformers = [
   mdxToJSTransformer,
-  createServeStorybookTransformer({ storybookConfigDir: config.storybookServerConfig['storybook-config'] }),
+  createServePreviewTransformer(assets),
   ...(config.esDevServerConfig.responseTransformers || []),
 ];
 
